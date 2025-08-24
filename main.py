321f85f074de890c42d229e18269792192f31c5b
Al Etienne <al@alandkimber.com>
2025-08-24 12:08:06 -0400

    interactive prompt

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 def main():
-    print("Hello from ai-agent!")
     client = genai.Client(api_key=api_key)
     model="gemini-2.0-flash-001"
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    if len(sys.argv) < 2:
+        print("Usage: python main.py <prompt>")
+        sys.exit(1)
+    contents=" ".join(sys.argv[1:])
+    print(contents)
+    #sys.exit(0)
     response = client.models.generate_content(model=model, contents=contents)
     print(response.text)
     print("Prompt tokens: " + str(response.usage_metadata.prompt_token_count))
